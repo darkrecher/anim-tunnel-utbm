f25167138c2b8865457752493ee6916b893688c0
Réchèr <recher@recher.com>
2019-10-12 11:47:07 +0200

    Tunnel avec texture, mais qui n'avance pas.

--- tunnel.py
+++ tunnel.py
@@ -57,8 +57,8 @@ data_dir = os.path.join(main_dir, 'data')
 screen_size_x = 400
 screen_size_y = 300
 
-tunnel_diameter = 350
-dist_focale = 200
+tunnel_diameter = 800
+dist_focale = 100
 
 
 def load_image(name, colorkey=None):
@@ -76,7 +76,7 @@ def load_image(name, colorkey=None):
     return image, image.get_rect()
 
 
-def cylinder_coords_from_screen_cords(pix_x, pix_y):
+def cylinder_coords_from_screen_coords(pix_x, pix_y):
     a_x = pix_x - screen_size_x/2
     a_y = pix_y - screen_size_y/2
     if (a_x, a_y) == (0, 0):
@@ -87,9 +87,9 @@ def cylinder_coords_from_screen_cords(pix_x, pix_y):
     return angle, cylinder_z
 
 
-def colorval_from_screen_cords(pix_x, pix_y, offset_z_cylinder=0):
+def colorval_from_screen_coords_old(pix_x, pix_y, offset_z_cylinder=0):
     try:
-        angle, cylinder_z = cylinder_coords_from_screen_cords(pix_x, pix_y)
+        angle, cylinder_z = cylinder_coords_from_screen_coords(pix_x, pix_y)
     except ValueError:
         return 0
     cylinder_z += offset_z_cylinder
@@ -98,6 +98,29 @@ def colorval_from_screen_cords(pix_x, pix_y, offset_z_cylinder=0):
     red = int(cylinder_z) & 255
     green = 0
     return red<<16 | green<<8 | blue
+
+
+def colorval_from_screen_coords(pix_x, pix_y, texture_width, texture_height, texture, offset_z_cylinder=0):
+    try:
+        angle, cylinder_z = cylinder_coords_from_screen_coords(pix_x, pix_y)
+    except ValueError:
+        return 0
+    cylinder_z += offset_z_cylinder
+    tex_x = int(cylinder_z % texture_width)
+    tex_y = int(((angle+math.pi)*(texture_height-1))/(2*math.pi))
+    tex_color = texture.get_at((tex_x, tex_y))
+    return tex_color.r<<16 | tex_color.g<<8 | tex_color.b
+
+
+def tex_coord_from_screen_coords(pix_x, pix_y, texture_width, texture_height):
+    try:
+        angle, cylinder_z = cylinder_coords_from_screen_coords(pix_x, pix_y)
+    except ValueError:
+        return (0, 0)
+    tex_x = int(cylinder_z % texture_width)
+    tex_y = int(((angle+math.pi)*(texture_height-1))/(2*math.pi))
+    return (tex_x, tex_y)
+
 
 
 def cycle_red(colorval):
@@ -118,6 +141,16 @@ def main():
     pygame.display.set_caption('Tunnel 3D')
     pygame.mouse.set_visible(0)
 
+    texture, texture_rect = load_image("texture.png")
+    print(texture_rect)
+    texture_width = texture_rect.w
+    texture_height = texture_rect.h
+
+    array_texture = pygame.surfarray.array2d(texture)
+    print(type(array_texture))
+    print(array_texture.dtype)
+    print(array_texture[0,0])
+
     # Create The Backgound
     background = pygame.Surface(screen.get_size())
     background = background.convert()
@@ -132,12 +165,12 @@ def main():
     #for x in range(screen_size_x):
     #    for y in range(screen_size_y):
     #        #background.set_at((x, y), (timer_color & 255, 0, 0))
-    #        background.set_at((x, y), color_from_screen_cords(x, y, timer_tick))
+    #        background.set_at((x, y), color_from_screen_coords(x, y, timer_tick))
     #background.unlock()
 
     for x in range(screen_size_x):
         for y in range(screen_size_y):
-            surf_array_background[x, y] = colorval_from_screen_cords(x, y, timer_tick)
+            surf_array_background[x, y] = colorval_from_screen_coords(x, y, texture_width, texture_height, texture, timer_tick)
 
     del surf_array_background
 
@@ -155,16 +188,16 @@ def main():
     #    current_screen.lock()
     #    for x in range(screen_size_x):
     #        for y in range(screen_size_y):
-    #            current_screen.set_at((x, y), color_from_screen_cords(x, y, index_color))
+    #            current_screen.set_at((x, y), color_from_screen_coords(x, y, index_color))
     #    current_screen.unlock()
     #    if index_color % 10 == 0:
     #        print(index_color)
 
-    array_pixel = numpy.zeros((screen_size_x, screen_size_y))
+    #array_pixel = numpy.zeros((screen_size_x, screen_size_y))
 
-    for x in range(screen_size_x):
-        for y in range(screen_size_y):
-            array_pixel[x, y] = colorval_from_screen_cords(x, y, timer_tick)
+    #for x in range(screen_size_x):
+    #    for y in range(screen_size_y):
+    #        array_pixel[x, y] = colorval_from_screen_coords(x, y, timer_tick)
 
     while going:
 
@@ -184,22 +217,23 @@ def main():
 
         #new_array_pixel = numpy.array([cycle_red(colorval) for colorval in array_pixel])
         #new_array_pixel = numpy.array([colorval+1 for colorval in array_pixel])
-        new_array_pixel = v_cycle_red(array_pixel)
-        array_pixel = new_array_pixel
+
+        #new_array_pixel = v_cycle_red(array_pixel)
+        #array_pixel = new_array_pixel
 
         #for x in range(screen_size_x):
         ##for x in range(190, 220):
         #    for y in range(screen_size_y):
         #        #array_pixel[x, y] = 256*65536 + 200*256 + timer_tick
-        #        array_pixel[x, y] = colorval_from_screen_cords(x, y, timer_tick)
+        #        array_pixel[x, y] = colorval_from_screen_coords(x, y, timer_tick)
 
 
-        pygame.surfarray.blit_array(screen, array_pixel)
+        #pygame.surfarray.blit_array(screen, array_pixel)
 
         #surf_array_background =  pygame.surfarray.pixels2d(background)
         #for x in range(screen_size_x):
         #    for y in range(screen_size_y):
-        #        surf_array_background[x, y] = colorval_from_screen_cords(x, y, timer_tick)
+        #        surf_array_background[x, y] = colorval_from_screen_coords(x, y, timer_tick)
         #del surf_array_background
 
         # Draw Everything
