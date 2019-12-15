18e6539c4f3791d62b7fadb02a8bfd283f782ddb
Recher <recher@plouf.com>
2019-12-16 00:00:35 +0100

    Rangement du code de la classe 'Boulette'.

--- code/boulette.py
+++ code/boulette.py
@@ -11,6 +11,8 @@ class Boulette(AnimationObject):
 
     DEG_TO_RAD_FACTOR = (2*math.pi) / 360
 
+    IMG_ORIGINAL_SCALE_DIVISOR = 4
+
     # Il faut mettre un diviseur de 360, sinon ça ne va pas tomber juste.
     IMG_ROTATION_PRECISION = 15
 
@@ -44,11 +46,16 @@ class Boulette(AnimationObject):
         img_boulette_originals = []
 
         for img_index in range(Boulette.NB_IMG_BOULETTES):
-            img_boulette, boul_rect = load_image("boulette_0" + str(img_index+1) + ".png", use_alpha=True)
-            boul_rect = pygame.Rect(0, 0, int(boul_rect.w*0.25), int(boul_rect.h*0.25))
+
+            filename_img = "boulette_0" + str(img_index + 1) + ".png"
+            img_boulette, boul_rect = load_image(filename_img, use_alpha=True)
+
             boulette_scaled = pygame.transform.scale(
                 img_boulette,
-                (boul_rect.w, boul_rect.h),
+                (
+                    boul_rect.w // Boulette.IMG_ORIGINAL_SCALE_DIVISOR,
+                    boul_rect.h // Boulette.IMG_ORIGINAL_SCALE_DIVISOR
+                ),
             )
             img_boulette_originals.append(boulette_scaled)
 
@@ -59,22 +66,28 @@ class Boulette(AnimationObject):
         # Le premier elem du sous-sous-tuple est l'image elle-même.
         # Le deuxième est la taille de l'image.
         # Le troisième est le vecteur (x, y) de décalage.
-        boulette_imgs_all = []
+        self.boulette_imgs_all = []
+
         for img_boulette_original in img_boulette_originals:
             boulette_imgs = []
             for angle in range(0, 360, Boulette.IMG_ROTATION_PRECISION):
-                vect_center_to_middle_down = pygame.math.Vector2(0, boul_rect.h//2)
+                vect_center_to_middle_down = pygame.math.Vector2(
+                    0,
+                    (boul_rect.h // Boulette.IMG_ORIGINAL_SCALE_DIVISOR) // 2
+                )
                 # Je sais pas pourquoi faut mettre "-angle" et pas "angle".
                 # Sûrement un truc compliqué de matheux.
                 vect_center_to_middle_down = vect_center_to_middle_down.rotate(-angle)
                 boulette_rotated = pygame.transform.rotate(img_boulette_original, angle)
                 rect_rotated = boulette_rotated.get_rect()
                 coord_hotpoint = (
-                    -rect_rotated.w//2 - vect_center_to_middle_down.x,
-                    -rect_rotated.h//2 - vect_center_to_middle_down.y
+                    -rect_rotated.w // 2 - vect_center_to_middle_down.x,
+                    -rect_rotated.h // 2 - vect_center_to_middle_down.y
                 )
                 boulette_imgs.append((boulette_rotated, rect_rotated, coord_hotpoint))
-            boulette_imgs_all.append(boulette_imgs)
+            self.boulette_imgs_all.append(boulette_imgs)
+
+        self.nb_rotation_img = len(self.boulette_imgs_all[0])
 
         # On sépare l'état random de la boulette de l'état random de tout le reste.
         # Comme ça les nombres aléatoires des déplacements de la boulette sont gérés
@@ -85,9 +98,6 @@ class Boulette(AnimationObject):
         random.seed("Des morceaux de l'uuuutééééébéééééhèèèèèème !")
         self.my_randgen_state = random.getstate()
         random.setstate(other_state)
-
-        self.boulette_imgs_all = boulette_imgs_all
-        self.len_boulette_imgs_rot = len(self.boulette_imgs_all[0])
 
         self.angle = Boulette.ANGLE_START
         # On fait exprès de partir d'une distance de 0: le centre de l'écran.
@@ -100,7 +110,7 @@ class Boulette(AnimationObject):
         self.main_img_index = 0
 
 
-    def _change_polar_coords(self):
+    def _update_polar_coords(self):
 
         other_state = random.getstate()
         random.setstate(self.my_randgen_state)
@@ -111,34 +121,29 @@ class Boulette(AnimationObject):
         self.my_randgen_state = random.getstate()
         random.setstate(other_state)
 
-        if self.delta_angle < -Boulette.DELTA_ANGLE_MAX and accel_angle < 0:
+        if any((
+            self.delta_angle < -Boulette.DELTA_ANGLE_MAX and accel_angle < 0,
+            self.delta_angle > Boulette.DELTA_ANGLE_MAX and accel_angle > 0,
+        )):
             accel_angle = -accel_angle * Boulette.ACCEL_ANGLE_LIMITING_FACTOR
-            #print("lim down", accel_angle, self.delta_angle, self.angle)
-        if self.delta_angle > Boulette.DELTA_ANGLE_MAX and accel_angle > 0:
-            accel_angle = -accel_angle * Boulette.ACCEL_ANGLE_LIMITING_FACTOR
-            #print("lim up", accel_angle, self.delta_angle, self.angle)
+
         self.delta_angle += accel_angle
         self.angle += self.delta_angle
-        if self.angle < 0:
-            self.angle += 360
-        if self.angle > 360:
-            self.angle -= 360
+        if self.angle < 0: self.angle += 360
+        if self.angle > 360: self.angle -= 360
 
         self.delta_dist += accel_dist
-        # Du coup: pas de garantie absolu que dist ne va pas sortir de [dist_min, dist_max]
-        # Mais osef.
-        if self.delta_dist < -Boulette.DELTA_DIST_MAX and accel_dist < 0:
+        # Du coup: pas de garantie absolue que dist ne va pas sortir de
+        # [dist_min, dist_max]. Mais osef.
+        if any((
+            self.delta_dist < -Boulette.DELTA_DIST_MAX and accel_dist < 0,
+            self.dist < Boulette.DIST_MIN and accel_dist < 0,
+            self.delta_dist > Boulette.DELTA_DIST_MAX and accel_dist > 0,
+            self.dist > Boulette.DIST_MAX and accel_dist > 0,
+        )):
             self.delta_dist -= accel_dist * Boulette.ACCEL_DIST_LIMITING_FACTOR
-        elif self.dist < Boulette.DIST_MIN and accel_dist < 0:
-            self.delta_dist -= accel_dist * Boulette.ACCEL_DIST_LIMITING_FACTOR
-        if self.delta_dist > Boulette.DELTA_DIST_MAX and accel_dist > 0:
-            self.delta_dist -= accel_dist * Boulette.ACCEL_DIST_LIMITING_FACTOR
-        elif self.dist > Boulette.DIST_MAX and accel_dist > 0:
-            self.delta_dist -= accel_dist * Boulette.ACCEL_DIST_LIMITING_FACTOR
-        self.dist += self.delta_dist
 
-        #print(accel_angle, self.delta_angle, self.angle)
-        #print(accel_dist, self.delta_dist, self.dist)
+        self.dist += self.delta_dist
 
 
     def _update_main_img_counter(self):
@@ -152,20 +157,22 @@ class Boulette(AnimationObject):
 
 
     def _get_cartez_coords(self):
+        angle_radian = self.angle * Boulette.DEG_TO_RAD_FACTOR
         return (
-            int(screen_size_x//2 + math.cos(self.angle*Boulette.DEG_TO_RAD_FACTOR)*self.dist),
-            int(screen_size_y//2 + math.sin(self.angle*Boulette.DEG_TO_RAD_FACTOR)*self.dist)
+            int(screen_size_x // 2 + math.cos(angle_radian) * self.dist),
+            int(screen_size_y // 2 + math.sin(angle_radian) * self.dist)
         )
 
 
     def _get_img_and_coords(self):
 
         boulette_imgs_current = self.boulette_imgs_all[self.main_img_index]
+        # TODO : va falloir expliquer ce bazar.
+        adjusted_angle = 360 - self.angle + Boulette.IMG_ROTATION_PRECISION / 2 + 90
 
-        index_boulette_rotation = int(( (360 -(self.angle -Boulette.IMG_ROTATION_PRECISION/2 -90)) / 360) * self.len_boulette_imgs_rot)
-
-        if index_boulette_rotation >= self.len_boulette_imgs_rot:
-            index_boulette_rotation -= self.len_boulette_imgs_rot
+        index_boulette_rotation = int((adjusted_angle * self.nb_rotation_img) / 360)
+        if index_boulette_rotation >= self.nb_rotation_img:
+            index_boulette_rotation -= self.nb_rotation_img
 
         scale_factor = (
             Boulette.SCALE_FACTOR_OFFSET + self.dist / Boulette.SCALE_FACTOR_DIVISOR
@@ -173,14 +180,15 @@ class Boulette(AnimationObject):
         boulette_rotated, rect_rotated, coord_hotpoint = boulette_imgs_current[
             index_boulette_rotation
         ]
+
         boulette_scaled = pygame.transform.scale(
             boulette_rotated,
-            (int(rect_rotated.w*scale_factor), int(rect_rotated.h*scale_factor))
+            (int(rect_rotated.w * scale_factor), int(rect_rotated.h * scale_factor))
         )
         boulette_coords = self._get_cartez_coords()
         coord_img_left_up = (
-            int(boulette_coords[0] + coord_hotpoint[0]*scale_factor),
-            int(boulette_coords[1] + coord_hotpoint[1]*scale_factor)
+            int(boulette_coords[0] + coord_hotpoint[0] * scale_factor),
+            int(boulette_coords[1] + coord_hotpoint[1] * scale_factor)
         )
         return boulette_scaled, coord_img_left_up
 
@@ -189,8 +197,8 @@ class Boulette(AnimationObject):
         """
         Gestion de la boulette de papier.
         """
-        self._change_polar_coords()
+        self._update_polar_coords()
         self._update_main_img_counter()
         boulette_scaled, coord_img_left_up = self._get_img_and_coords()
-        screen.blit(boulette_scaled, (coord_img_left_up[0], coord_img_left_up[1]))
+        screen.blit(boulette_scaled, coord_img_left_up)
 
